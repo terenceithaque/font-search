2cfcc5b3136f12b092ac41e677dcd83b7f02ec86
terenceithaque <terence.lethierry@protonmail.com>
2023-10-25 10:35:43 +0200

    Ajout label et barre de recherche

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ url = "https/www.googleapis.com/webfonts/v1/webfonts"
 
 class Application(Tk):
     "Application"
+
+    def __init__(self):
+        super().__init__()
+        # Demander à l'utilisateur de saisir une police d'écriture
+        self.label_recherche = Label(
+            self, text="Saisissez une police d'écriture:")
+        self.label_recherche.pack()
+        # Champ de texte pour permettre à l'utilisateur d'effectuer sa recherche
+        self.barre_recherche = Entry(self)
+        # Faire s'étendre la barre de recherche sur toute la longueur de la fenêtre
+        self.barre_recherche.pack(fill="x")
+
+
+app = Application()  # Application
+app.mainloop()  # Démarrer l'application
